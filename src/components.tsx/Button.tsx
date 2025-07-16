b2683aad95da5d1b4d2915812fa2e8394cc8df9f
prushton2 <peterrushton418@gmail.com>
2025-07-16 01:47:41 -0400

    minor fixes

--- src/components.tsx/Button.tsx
+++ src/components.tsx/Button.tsx
@@ -3,7 +3,7 @@ import "../global.css";
 import { useEffect, useState, type JSX } from 'react';
 import type { Indicators } from "./Setup";
 
-function Button({ batteries, indicators }: { batteries: number, indicators: Indicators}): JSX.Element {
+function Button({ batteries, indicators }: { batteries: number, indicators: Indicators }): JSX.Element {
     const [label, setLabel] = useState("")
     const [color, setColor] = useState("")
     const [strip, setStrip] = useState("")
@@ -15,50 +15,52 @@ function Button({ batteries, indicators }: { batteries: number, indicators: Indi
     const labels: string[] = ["Abort", "Detonate", "Hold", "Other"]
     const colors: string[] = ["Red", "Green", "Blue", "Yellow", "White", "Black"]
 
-    function solveStrip(color: string): string {
-        if (color == "Blue") {
-            return "Release when the timer has a 4 in any position"
-        }
-        if (color == "Yellow") {
-            return "Release when the timer has a 5 in any position"
-        }
-        return "Release when the timer has a 1 in any position"
-    }
-
     function solve(): string {
+        function solveStrip(color: string): string {
+            if (color == "Blue") {
+                return "Release when the timer has a 4 in any position"
+            }
+            if (color == "Yellow") {
+                return "Release when the timer has a 5 in any position"
+            }
+            return "Release when the timer has a 1 in any position"
+        }
+
+        function releaseHeldStrip(): string {
+            if (strip == "") {
+                return "Press and hold button, then select the strip color"
+            }
+            return `If you have a ${strip} strip, then ${solveStrip(strip)}`
+
+        }
+
+        if (color == "" || label == "") {
+            return "Select a color and label"
+        }
+
         if (color == "Blue" && label == "Abort") {
-            if (strip == "") {
-                return "Press and hold button, then select the strip color"
-            }
-            return `If you have a ${strip} strip, then ${solveStrip(strip)}`
+            return releaseHeldStrip()
         }
 
-        if(batteries > 1 && label == "Detonate") {
+        if (batteries > 1 && label == "Detonate") {
             return "Press and immediately release the button"
         }
 
-        if(color == "White" && indicators.lit.CAR) {
-            if (strip == "") {
-                return "Press and hold button, then select the strip color"
-            }
-            return `If you have a ${strip} strip, then ${solveStrip(strip)}`
+        if (color == "White" && indicators.lit.CAR) {
+            return releaseHeldStrip()
         }
 
-        if(batteries > 2 && indicators.lit.FRK) {
+        if (batteries > 2 && indicators.lit.FRK) {
             return "Press and immediately release the button"
         }
 
-        if(color == "Yellow") {
-            if (strip == "") {
-                return "Press and hold button, then select the strip color"
-            }
-            return `If you have a ${strip} strip, then ${solveStrip(strip)}`
+        if (color == "Yellow") {
+            return releaseHeldStrip()
+
         }
 
-        if (strip == "") {
-            return "Press and hold button, then select the strip color"
-        }
-        return `If you have a ${strip} strip, then ${solveStrip(strip)}`
+        return releaseHeldStrip()
+
 
     }
 
@@ -72,7 +74,13 @@ function Button({ batteries, indicators }: { batteries: number, indicators: Indi
                         return <button
                             key={`button.label.${e}`}
                             className={`WireButton ${label == e ? "Red" : "Empty"}`}
-                            onClick={() => setLabel(e)}
+                            onClick={() => {
+                                if (e == label) {
+                                    setLabel("")
+                                } else {
+                                    setLabel(e)
+                                }
+                            }}
                         >
                             {e}
                         </button>
@@ -84,7 +92,13 @@ function Button({ batteries, indicators }: { batteries: number, indicators: Indi
                         return <button
                             key={`button.color.${e}`}
                             className={`WireButton ${e == color ? e : "Empty"}`}
-                            onClick={() => setColor(e)}
+                            onClick={() => {
+                                if (e == color) {
+                                    setColor("")
+                                } else {
+                                    setColor(e)
+                                }
+                            }}
                         >
                             {e}
                         </button>
@@ -97,12 +111,12 @@ function Button({ batteries, indicators }: { batteries: number, indicators: Indi
                         return <button
                             key={`strip.color.${e}`}
                             className={`WireButton ${e == strip ? e : "Empty"}`}
-                            onClick={() => { 
-                                if (e == strip) { 
-                                    setStrip("") 
-                                } else { 
-                                    setStrip(e) 
-                                } 
+                            onClick={() => {
+                                if (e == strip) {
+                                    setStrip("")
+                                } else {
+                                    setStrip(e)
+                                }
                             }}
                         >
                             {e}
@@ -111,8 +125,8 @@ function Button({ batteries, indicators }: { batteries: number, indicators: Indi
                     })}
                 </div>
             </div>
-            <div className="ButtonComponent">
-                    {solve()}
+            <div>
+                <label>{solve()}</label>
             </div>
         </>
 
